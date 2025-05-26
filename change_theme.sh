fadb460e429eafda112757252c5ee4d7dd61aaa0
Wiener234 <wiemernils@gmail.com>
2025-05-26 21:46:33 +0200

    add check at boot

--- change_theme.sh
+++ change_theme.sh
@@ -6,6 +6,15 @@ LONGITUDE=13.41053E     # Replace with your longitude
 light="echo false > $HOME/.config/cosmic/com.system76.CosmicTheme.Mode/v1/is_dark"  # Replace with your desired command
 dark="echo true > $HOME/.config/cosmic/com.system76.CosmicTheme.Mode/v1/is_dark"
 
+# Set theme immediately based on current sun position
+if [ "$(sunwait poll rise set $LATITUDE $LONGITUDE)" = "DAY" ]; then
+    echo "It's daytime, setting light theme"
+    eval "$light"
+else
+    echo "It's nighttime, setting dark theme"
+    eval "$dark"
+fi
+
 while true; do
     # Wait until next sunrise
     echo "Waiting for sunrise..."
